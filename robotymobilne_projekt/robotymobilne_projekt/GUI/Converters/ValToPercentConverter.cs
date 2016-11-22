57e9422d13e6aa6918ba77b3400398eac229a572
Flajss <tnt33dexter@gmail.com>
2016-11-22 19:34:11 +0100

    Update Baterry Status

---
+++ robotymobilne_projekt/robotymobilne_projekt/GUI/Converters/ValToPercentConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace robotymobilne_projekt.GUI.Converters
+{
+    public class ValToPercentConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is double)
+            {
+                double toConvert = (double)value;
+                return mapValues(toConvert, 4300, 5000, 0, 100);
+            }
+            return 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double)
+            {
+                double toConvert = (double)value;
+                return mapValues(toConvert, 0, 100, 4300, 5000);
+            }
+            return 0;
+        }
+
+        private int mapValues(double value, double fromSource, double toSource, double fromTarget, double toTarget)
+        {
+            return (int)((value - fromSource) / (toSource - fromSource) * (toTarget - fromTarget) + fromTarget);
+        }
+    }
+}
